1f5ab48dfa567b559f572c1e27172ca8f353de3d
eb18 <56936793+eb18@users.noreply.github.com>
2020-02-07 20:57:23 -0500

    Angry
    added hubHubGitHubSuckz();

--- src/githubcollaboration/GitHubCollaboration.java
+++ src/githubcollaboration/GitHubCollaboration.java
@@ -14,12 +14,17 @@ public class GitHubCollaboration {
     public static void GitHubIsAnnoying(){
         System.out.println("I hate GitHub.");
     }
+    
+    public static void hubHubGitHubSucks(){
+        System.out.println("GitHub makes me angry ┗(｀Дﾟ┗(｀ﾟДﾟ´)┛ﾟД´)┛ ");
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         GitHubIsAnnoying();
+        hubHubGitHubSucks();
     }
     
 }
